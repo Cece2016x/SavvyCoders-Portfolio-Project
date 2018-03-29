3bdb6565e0f2135d66cedd1af4ce64a50f82847e
Celia Bourrage <Celia Bourrage>
2018-03-28 20:35:22 -0500

    change layout to blog page

--- js/blog.js
+++ js/blog.js
@@ -1,6 +1,25 @@
 /* globals $ */
 
+var $footer = $( "#footer" );
 var getPosts = $.ajax( "https://jsonplaceholder.typicode.com/posts" );
 
+function generatePosts( posts ){
+    var $postsList = $( "<p2>" );
+    var $postItems = posts
+        .filter(
+            ( post ) => post.id < 11
+        )
+        .map(
+            ( post ) => $( "<p2>" ).text( post.title )
+        );
 
-getPosts.then( ( posts ) => console.log( posts[0] ) );
+    return $postsList.append( $postItems );
+}
+
+function appendPosts( posts ){
+    var $posts = generatePosts( posts );
+
+    $footer.append( $posts );
+}
+
+getPosts.then( appendPosts );
